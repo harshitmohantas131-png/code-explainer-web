b95e3c357e492dcc9726301fd47a9f83c742c07f
Harshit Mohanta <harshit.mohanta.s.131@kalvium.community>
2026-04-17 12:15:17 +0530

    fix: proper fetch import using ES modules

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
