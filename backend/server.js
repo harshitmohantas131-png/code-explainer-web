c4d2495c6bc283719e983bc35a6dfe552f3c8c7b
Harshit Mohanta <harshit.mohanta.s.131@kalvium.community>
2026-04-17 12:11:21 +0530

    fix: fetch not defined

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 dotenv.config();
 
